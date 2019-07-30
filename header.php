5b219f641f3db6daf4b1a40e5f0911b2e75e75b5
V744 <vivekmalhotra676@gmail.com>
2019-07-31 02:12:05 +0530

    First commit

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Library</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- JS -->
+    <script type="text/javascript" src="js/jquery_1.3.2.js"></script>
+    
+    <script type="text/javascript" src="js/jquery.cycle.all.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <!-- ENDS JS -->
+    <!-- superfish -->
+    <link rel="stylesheet"  type="text/css" media="screen" href="css/superfish-custom.css" />
+    <script type="text/javascript" src="js/superfish-1.4.8/js/hoverIntent.js"></script>
+    <script type="text/javascript" src="js/superfish-1.4.8/js/superfish.js"></script>
+    <!-- ENDS superfish -->
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/spring.css" type="text/css" media="screen" />
+
+    <!-- ENDS CSS -->
+    <!-- Cufon -->
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/bebas_400.font.js" type="text/javascript"></script>
+    <script type="text/javascript">Cufon.replace('.custom', { fontFamily: 'bebas', hover: true });</script>
+    <!-- /Cufon -->
+</head>
+<body>
+    <!-- WRAPPER -->
+    <div id="wrapper">    
+      <!-- navigation -->
+      <ul id="nav" class="sf-menu">    
+          <li class="custom"><a href="javascript:;">CONTACT</a></li>
+          <li class="custom"><a href="javascript:;">ABOUT</a></li>
+          <li class="custom"><a href="javascript:;">GALLERY</a></li>
+
+          <li class="custom"><a href="index.php">HOME</a>
+          </ul>
+          <!-- ENDS navigation -->
+          <!-- HEADER -->
+          <div id="header"> <a href="index.php"><img src="img/logo.png" alt="" id="logo" /></a>
+
+          </div>
+          <!-- ENDS HEADER -->			
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('header.php'); ?>
+<!-- MAIN -->
+<div id="main">
+  <!-- slideshow -->
+  <div id="slideshow">
+    <ul id="slides">
+      <li><img src="slides/01.jpg" alt="" /></li>
+      <li><img src="slides/02.jpg" alt="" /></li>
+      <li><img src="slides/03.jpg" alt="" /></li>
+      
+    </ul>
+    <!-- ENDS slideshow -->
+  </div>
+  <!-- ENDS MAIN -->
+</div>
+
