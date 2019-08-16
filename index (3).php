84c78fa731b35cf2cf76b0286775df4e42540c7a
V744 <vivekmalhotra676@gmail.com>
2019-08-16 08:44:56 +0530

    Index Updated

---
+++ index (3).php
@@ -0,0 +1,59 @@
+<?php include('header.php'); ?>
+<!-- MAIN -->
+<div id="main">
+  <!-- slideshow -->
+  <div id="slideshow">
+    <ul id="slides">
+      <li><img src="slides/01.jpg" alt="" /></li>
+      <li><img src="slides/02.jpg" alt="" /></li>
+      <li><img src="slides/03.jpg" alt="" /></li>
+      <li><img src="slides/05.jpg" alt="" /></li>
+      <li><img src="slides/07.jpg" alt="" /></li>
+      
+      
+    </ul>
+    <a href="#"><img src="img/prev-slide.png" alt="" id="prev" /></a> <a href="#"><img src="img/next-slide.png" alt="" id="next" /></a> </div>
+    <!-- ENDS slideshow -->
+
+
+    <!-- blocks -->
+    <div class="holder">
+      <div class="block">
+
+        <h2 class="custom color2"><center>KIDS CORNER</center></h2>
+
+        <p class="thumb-text"><center>A book library of kid's books from  and authors from around the world! Whether your child is just learning their ABCs or learning another language after school, they can travel further with The Reading House.</center>
+
+        </p>
+        
+        <P style="border-bottom: 3px solid #665d4e;"></P>
+
+      </div>
+      
+
+      <div class="block">
+
+        <h2 class="custom color2"><center>YOUNG ZONE</center></h2>
+        <p class="thumb-text"><center>Information  even can be shared by colleage students much more easily. The Reading House encourage youth to visit new worlds, discover new points of view, and keep building upon their knowledge
+        </center></p>
+        <P style="border-bottom: 3px solid #665d4e;"></P>        
+      </div>
+
+      <div class="block last">
+
+        <h2 class="custom color2"><center>OLD AGE</center></h2>
+        
+        <p class="thumb-text"><center>The Reading House is also open to all senior citizens.Education is the &nbsp;best provision for the old age journey. They can easily access the information they need and quikly download books to read.</center> </p> 
+        <P style="border-bottom: 3px solid #665d4e;"></P>
+        
+      </div>
+      
+
+      <!-- ENDS blocks -->
+
+
+    </div>
+    <!-- ENDS MAIN -->
+  </div>
+  <?php include('footer.php');?>
+
