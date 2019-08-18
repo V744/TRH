321f63ccc4624c52f62a18ac8943d382ef0e4a60
sushmithavootla <sushmithavootla2@gmail.com>
2019-08-18 18:34:04 -0400

    footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+</div>
+<!-- FOOTER -->
+<div id="footer">
+	<div id="footer-wrapper">
+		<ul id="follow">
+			<li>
+				<p>Quick links: </p>
+			</li>
+			<li><a href="https://twitter.com/"><img src="img/twitter.png" alt="" /></a></li>
+			<li><a href="https://www.facebook.com/"><img src="img/facebook.png" alt="" /></a></li>
+			<li><a href="https://www.linkedin.com/"><img src="img/linkedln.png" alt="" /></a></li>			
+		</ul>    
+		<div class="footer-bottom">
+			<!-- <p class="legal">&copy; Copyright 2019 <a href="#">Reading Books</a> All Rights Reserved.</p>-->
+		</div>
+			</div> 
+		</div>
+	
+	<!-- ENDS FOOTER -->
+	<!-- start cufon -->
+	<script type="text/javascript">Cufon.now();</script>
+	<!-- ENDS start cufon -->
+</body>
+</html>				
